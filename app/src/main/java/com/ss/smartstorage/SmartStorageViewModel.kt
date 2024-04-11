249717fb0b5dee3cadaf609417fc47241d7372f2
harshilpadsala88 <harshil.padsala@smartsensesolutions.com>
2024-04-11 17:53:40 +0530

    Minor Change

--- app/src/main/java/com/ss/smartstorage/SmartStorageViewModel.kt
+++ app/src/main/java/com/ss/smartstorage/SmartStorageViewModel.kt
@@ -1,7 +1,9 @@
 package com.ss.smartstorage
 
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.lifecycle.ViewModel
 
 class SmartStorageViewModel : ViewModel() {
+    private val state = mutableIntStateOf(0)
 
 }
