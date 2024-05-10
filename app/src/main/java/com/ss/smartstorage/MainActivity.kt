69ce1b553ba3934df0ae223bc096f9bdc11aa993
Dhruvi-sS <dhruvi.chotai@smartsensesolutions.com>
2024-05-10 15:06:20 +0530

    Sonarlint issue resolved

--- app/src/main/java/com/ss/smartstorage/MainActivity.kt
+++ app/src/main/java/com/ss/smartstorage/MainActivity.kt
@@ -9,8 +9,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.ss.smart_storage.SmartStorage
-import com.ss.smart_storage.util.SmartDirectory
-import com.ss.smart_storage.util.SmartFileType
 import com.ss.smartstorage.ui.theme.SmartStorageTheme
 
 class MainActivity : ComponentActivity() {
