193822e392e13a5aadb51914703456c1b990beac
Sagar Maiyad Smartsense <67956336+sagarsmartsense@users.noreply.github.com>
2024-05-10 16:15:00 +0530

    Revert "SmartStorage Demo Application  (#10)" (#11)
    This reverts commit adbb7317d50de227b09dd1bf9bf32ccad640d69a.

--- app/src/main/java/com/ss/smartstorage/MainActivity.kt
+++ app/src/main/java/com/ss/smartstorage/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.ss.smartstorage
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,40 +8,29 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.ss.smart_storage.SmartStorage
+import com.ss.smart_storage.util.SmartDirectory
 import com.ss.smartstorage.ui.theme.SmartStorageTheme
 
 class MainActivity : ComponentActivity() {
-
-    @SuppressLint("NewApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val smartStorage = SmartStorage(this)
-
-
         setContent {
             SmartStorageTheme {
+                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    SmartStorageSample(
-                        onStoreTap = { fileName, fileType, location ->
-                                smartStorage.store(
-                                    location = location,
-                                    fileType = fileType,
-                                    fileName = fileName,
-                                    fileData = "This is a sample txt file.".toByteArray()
-                                )
-                        }
-                   )
+
+                    val smartStorage = SmartStorage(context = applicationContext)
+                    smartStorage.store(
+                        location = SmartDirectory.EXTERNAL.downloads,
+                        fileData = byteArrayOf()
+                    )
+                    SmartStorageSample()
                 }
             }
         }
     }
-
-
-
-
 }
 
--- smart-storage/src/main/java/com/ss/smart_storage/util/SmartDirectory.kt
+++ smart-storage/src/main/java/com/ss/smart_storage/util/SmartDirectory.kt
@@ -1,10 +1,31 @@
 package com.ss.smart_storage.util
 
-object SmartDirectory{
-    const val INTERNAL: String = "Internal"
-    const val SCOPED_STORAGE : String = "Scoped_Storage"
-    const val CUSTOM : String = "Custom"
-    const val DOWNLOADS: String = "Download"
-    const val DOCUMENTS: String = "Documents"
-    const val EXTERNAL_PUBLIC = "External_Public"
+import android.os.Build
+import android.os.Environment
+import androidx.annotation.RequiresApi
+
+sealed class SmartDirectory{
+    companion object{
+        val INTERNAL : String = "Internal"
+    }
+
+    data object EXTERNAL : SmartDirectory() {
+        val music: String = Environment.DIRECTORY_MUSIC
+        val podcasts: String = Environment.DIRECTORY_PODCASTS
+        val ringtones: String = Environment.DIRECTORY_RINGTONES
+        val alarms: String = Environment.DIRECTORY_ALARMS
+        val notifications: String = Environment.DIRECTORY_NOTIFICATIONS
+        val pictures: String = Environment.DIRECTORY_PICTURES
+        val movies: String = Environment.DIRECTORY_MOVIES
+        val downloads: String = Environment.DIRECTORY_DOWNLOADS
+        val dcim: String = Environment.DIRECTORY_DCIM
+        val documents: String = Environment.DIRECTORY_DOCUMENTS
+
+        @RequiresApi(Build.VERSION_CODES.Q)
+        val audiobooks: String = Environment.DIRECTORY_AUDIOBOOKS
+
+        @RequiresApi(Build.VERSION_CODES.S)
+        val recordings: String = Environment.DIRECTORY_RECORDINGS
+    }
 }
+
