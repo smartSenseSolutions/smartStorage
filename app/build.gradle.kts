9d1d5e952d27d2b6c60eb625fbeba63a2244e2ce
Sagar Maiyad Smartsense <67956336+sagarsmartsense@users.noreply.github.com>
2024-05-10 16:14:09 +0530

    Revert "SmartStorage Demo Application  (#10)"
    This reverts commit adbb7317d50de227b09dd1bf9bf32ccad640d69a.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ android {
 }
 
 dependencies {
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -58,11 +59,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.coil.compose)
-    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(project(":smart-storage"))
-    implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.documentfile)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -70,8 +67,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-
-
 
     project(":smart-storage")
 
--- smart-storage/src/main/java/com/ss/smart_storage/util/PermissionStatus.kt
+++
@@ -1,5 +0,0 @@
-package com.ss.smart_storage.util
-
-enum class PermissionStatus {
-    ACCEPTED, DENIED, NOT_APPLICABLE, NOT_NEEDED, NOT_AVAILABLE, REDIRECT_TO_SETTINGS
-}
