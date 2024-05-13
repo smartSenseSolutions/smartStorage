c35628e844e3a920593191bebb2019adcd9ee269
Dhruvi-sS <dhruvi.chotai@smartsensesolutions.com>
2024-05-13 12:54:47 +0530

    SmartStorage applicable in Android 11 and above for External Public Directory and Refactored Code

--- smart-storage/src/main/java/com/ss/smart_storage/PermissionManager.kt
+++ smart-storage/src/main/java/com/ss/smart_storage/PermissionManager.kt
@@ -3,16 +3,19 @@ package com.ss.smart_storage
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Build
+import android.os.Build.VERSION_CODES.M
+import android.os.Build.VERSION_CODES.Q
+import android.os.Environment
 import androidx.activity.ComponentActivity
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import com.ss.smart_storage.util.PermissionStatus
 import com.ss.smart_storage.util.SmartDirectory
 
 
 class PermissionManager(
-    private val activity: ComponentActivity,
-    val onPermissionGranted: (PermissionStatus) -> Unit
+    private val activity: ComponentActivity, val onPermissionGranted: (PermissionStatus) -> Unit
 ) {
     private val requestPermissionLauncher = activity.registerForActivityResult(
         ActivityResultContracts.RequestPermission()
@@ -55,27 +58,22 @@ class PermissionManager(
 
     private fun checkOsForPermissions(location: String) {
         when {
-            isVersionInBetween(Build.VERSION_CODES.M, Build.VERSION_CODES.Q) -> {
-                checkIfPermissionGranted(
-                    onPermissionGranted = {
-                        onPermissionGranted(PermissionStatus.ACCEPTED)
-                    })
+            Build.VERSION.SDK_INT in M..Q -> {
+                checkIfPermissionGranted(onPermissionGranted = {
+                    onPermissionGranted(PermissionStatus.ACCEPTED)
+                })
             }
-            isVersionInBetween(
-                Build.VERSION_CODES.R, Build.VERSION_CODES.S
-            ) && location == SmartDirectory.EXTERNAL_PUBLIC -> {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                    checkIfManagePermissionGranted(
-                        onPermissionGranted = { granted ->
-                            if(granted)  onPermissionGranted(PermissionStatus.ACCEPTED)
-                            else onPermissionGranted(PermissionStatus.REDIRECT_TO_SETTINGS)
-                        })
-                }
+
+            (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) && location == SmartDirectory.EXTERNAL_PUBLIC -> {
+
+                checkIfManagePermissionGranted(onPermissionGranted = { granted ->
+                    if (granted) onPermissionGranted(PermissionStatus.ACCEPTED)
+                    else onPermissionGranted(PermissionStatus.REDIRECT_TO_SETTINGS)
+                })
+
 
             }
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU && location == SmartDirectory.EXTERNAL_PUBLIC -> {
-                onPermissionGranted(PermissionStatus.NOT_AVAILABLE)
-            }
+
             else -> {
                 onPermissionGranted(PermissionStatus.ACCEPTED)
             }
@@ -90,26 +88,17 @@ class PermissionManager(
         ) {
             onPermissionGranted()
         } else {
-            requestPermissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE,)
+            requestPermissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
         }
     }
 
-    private fun checkIfManagePermissionGranted( onPermissionGranted: (Boolean) -> Unit) {
-        if (ContextCompat.checkSelfPermission(
-                activity, Manifest.permission.MANAGE_EXTERNAL_STORAGE ,
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
+    @RequiresApi(Build.VERSION_CODES.R)
+    private fun checkIfManagePermissionGranted(onPermissionGranted: (Boolean) -> Unit) {
+        if (Environment.isExternalStorageManager()) {
             onPermissionGranted(true)
         } else {
             onPermissionGranted(false)
         }
-    }
-
-
-
-
-    private fun isVersionInBetween(min: Int, max: Int): Boolean {
-        return Build.VERSION.SDK_INT in min..max
     }
 
 
