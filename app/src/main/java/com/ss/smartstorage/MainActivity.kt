179e5b53b4747a93122de0d62a2d5333ed92521e
Sagar Maiyad Smartsense <67956336+sagarsmartsense@users.noreply.github.com>
2024-06-05 16:10:53 +0530

    Return storage path in listener added. (#17)
    Co-authored-by: sagar <sagar@LAP-089.local>

--- app/src/main/java/com/ss/smartstorage/MainActivity.kt
+++ app/src/main/java/com/ss/smartstorage/MainActivity.kt
@@ -2,24 +2,25 @@ package com.ss.smartstorage
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.ss.smart_storage.OutputListener
 import com.ss.smart_storage.SmartStorage
 import com.ss.smartstorage.ui.theme.SmartStorageTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), OutputListener {
 
     @SuppressLint("NewApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val smartStorage = SmartStorage(this)
-
-
+        smartStorage.registerListener(this)
         setContent {
             SmartStorageTheme {
                 Surface(
@@ -41,7 +42,15 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    override fun onSuccess(result: String?) {
+        result?.let { toast(it) }
+        Log.e("MainAct", "onSuccess result: $result")
+    }
 
+    override fun onFail(error: String?) {
+        error?.let { toast(it) }
+        Log.e("MainAct", "onFail error: $error")
+    }
 
 
 }
